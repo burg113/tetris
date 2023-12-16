ccae7db192bbf64a676c64502b980b35c8145e3c
Lucas Schwebler <lucas.schwebler@gmail.com>
2023-12-16 13:10:12 +0100

    Simplify protocol (always use 4 byte size)

--- src/networking/SocketWrapper.cpp
+++ src/networking/SocketWrapper.cpp
@@ -4,15 +4,10 @@
 
 #include "SocketWrapper.h"
 
-#include <utility>
-
 int SocketWrapper::numSessions = 0;
 
 SocketWrapper::SocketWrapper(asio::io_service &ioService)
         : socket(ioService), id(numSessions++) {
-    firstRead = true;
-    curHeader = 0;
-    lenlen = 0;
     len = 0;
     alive = true;
 }
@@ -38,27 +33,14 @@ void SocketWrapper::sendRaw(const std::string &str) {
     asio::write(socket, asio::buffer(str));
 }
 
-void SocketWrapper::send(uint8_t header, const std::string &str) {
-    assert(header < 64);
-    std::string msg(1, ' ');
-    header <<= 2;
-    if(str.size() == 1) header |= 1;
-    else if(1 < str.size() && str.size() <= 255){
-        header |= 2;
-        msg.resize(2);
-        msg[1] = static_cast<char>(str.size());
-    }
-    else if(256 <= str.size()){
-        header |= 3;
-        msg.resize(5);
-        uint32_t x = str.size();
-        for(int i = 4; i >= 1; i--){
-            msg[i] = static_cast<char>(x & 0xff);
-            x /= 256;
-        }
+void SocketWrapper::send(const std::string &str) {
+    std::string msg(4, '0');
+    uint32_t x = str.size();
+    for(int i = 3; i >= 0; i--){
+        msg[i] = static_cast<char>(x & 0xff);
+        x /= 256;
     }
     msg += str;
-    msg[0] = static_cast<char>(header);
     sendRaw(msg);
 }
 
@@ -93,39 +75,24 @@ void SocketWrapper::handleRead(const asio::error_code &err, size_t numBytes) {
     }
 //    std::cout << "Socked " << id << " received " << (int)buf[0] << std::endl;
     uint32_t oldLen = data.size();
-    if (firstRead) {
-        firstRead = false;
-        curHeader = static_cast<uint8_t>(buf[0]);
-        uint8_t lenInfo = curHeader & 3;
-        if(lenInfo == 3) lenlen = 4;
-        else if(lenInfo == 2) lenlen = 1;
-        else {
-            lenlen = 0;
-            len = lenInfo;
-        }
-        data += std::string(buf + 1, buf + numBytes);
-    }
-    else data += std::string(buf, buf + numBytes);
-    if (oldLen < lenlen && lenlen <= data.size()) {
-        for (int i = 0; i < lenlen; i++) {
+    data += std::string(buf, buf + numBytes);
+    if (oldLen < 4 && 4 <= data.size()) {
+        for (int i = 0; i < 4; i++) {
             len *= 256;
             len += static_cast<uint8_t>(data[i]);
         }
-        len += lenlen;
+        len += 4;
     }
-    if(data.size() >= lenlen) {
+    if(data.size() >= 4) {
         if (data.size() > len) {
             std::cerr << "WARNING: Socket " << id << " received message of length " << data.size() << " but header said that it has length " << len << std::endl;
         }
         if (data.size() >= len) {
             // handle message
             for(const SocketReadCallback &callback : readCallbacks) {
-                callback(this, curHeader >> 2, data.substr(lenlen));
+                callback(this, data.substr(4));
             }
             // reset state
-            firstRead = true;
-            curHeader = 0;
-            lenlen = 0;
             len = 0;
             data.clear();
         }
